69362c53af1b3d4e7047565d485b6f3ac8693f57
Christian <abel.christian.acc@gmail.com>
2024-12-26 19:13:04 +0100

    Todos

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+""" TODO: 
+    1. refactor folder structure for better overview
+    2. look for the other Fixmes & Todos in different files
+    3. add rest of communication as shown in communication.drawio.png
+    4. plan next features needed to continue
+"""
 # Imports
 from network import initNetwork
 from visuals import initVisuals
